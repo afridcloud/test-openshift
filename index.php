311d11ebc1fdf1dbed0799bbc24624c330d5a165
afridcloud <60093215+afridcloud@users.noreply.github.com>
2020-12-20 20:00:34 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+<pre>
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+
+$output = shell_exec('ifconfig');
+echo "$output";
+</pre>
+?>
