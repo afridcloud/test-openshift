226e959236265aa8f0955447ed0792ace4f1296b
afridcloud <60093215+afridcloud@users.noreply.github.com>
2020-12-20 20:04:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<?php
-<pre>
-$color = "red";
-echo "My car is " . $color . "<br>";
-echo "My house is " . $COLOR . "<br>";
-echo "My boat is " . $coLOR . "<br>";
 
-$output = shell_exec('ifconfig');
-echo "$output";
-</pre>
+<?php
+echo "Hello World!";
 ?>
